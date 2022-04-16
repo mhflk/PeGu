8fde05103292181d0426ac8e3386e25827a7b889
endhambot <endhambot@gmail.com>
2022-04-16 22:21:29 +0530

    scrollfixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,17 +84,14 @@ window.addEventListener('scroll', scrollActive)
 /*==================== SCROLL REVEAL ANIMATION ====================*/
 const sr = ScrollReveal({
     distance: '60px',
-    duration: 2800,
+    duration: 1200,
     // reset: true,
 })
 
 
 sr.reveal(`.home__data, .home__social-link, .home__info,
-           .discover__container,
-           .experience__data, .experience__overlay,
-           .place__card,
-           .sponsor__content,
-           .footer__data, .footer__rights`,{
+           
+           .experience__data`,{
     origin: 'top',
     interval: 100,
 })
